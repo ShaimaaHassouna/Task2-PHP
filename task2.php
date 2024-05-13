cb0ddfebc1ff4900959d2f3ddd07f4dd6f6fbc55
Shaimaa Ahmed Hassouna <82947295+ShaimaaHassouna@users.noreply.github.com>
2024-05-14 01:33:37 +0300

    Add files via upload

---
+++ task2.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hello PHP <br> <br>";
+$my_name = "Shaimaa Ahmed Gaber Hassouna";
+$birth_dete = "18, May, 1993";
+
+echo "My name is<strong> $my_name</strong> <br> <br>I was born in<b> $birth_dete</b>";
+echo "<br><br> Thank you "
+?>
